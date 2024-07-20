3add4db76ddca4a02951cf9044e9207d5e7cea96
denisolid <dkolisnichienko321@gmail.com>
2024-07-20 13:03:38 +0000

    load more button and functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,31 +2,51 @@ import { useEffect, useState } from "react";
 import List from "./List/List";
 import { fetchNews } from "../services/api";
 import { SearchBar } from "./SearchBar/SearchBar";
+import { LineWave } from "react-loader-spinner";
 
 export const App = () => {
   const [hits, setHits] = useState([]);
   const [query, setQuery] = useState("react");
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     const getData = async () => {
       try {
         setIsLoading(true);
-        const response = await fetchNews(query, 25);
-        setHits(response.hits);
+        setIsError(false);
+        const response = await fetchNews(query, 5);
+        setHits((prev) => [...prev, ...response.hits]);
       } catch (error) {
         console.log(error);
+        setIsError(true);
       } finally {
         setIsLoading(false);
       }
     };
     getData();
-  }, [query]);
+  }, [query, page]);
   return (
     <div>
       <SearchBar setQuery={setQuery} />
-      {isLoading && <p>Loading data...</p>}
+      {isLoading && (
+        <LineWave
+          visible={true}
+          height="100"
+          width="100"
+          color="#4fa94d"
+          ariaLabel="line-wave-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          firstLineColor=""
+          middleLineColor=""
+          lastLineColor=""
+        />
+      )}
+      {isError && <p>Something went wrong! Try again...</p>}
       <List items={hits} />
+      <button onClick={() => setPage((prev) => prev + 1)}>Load more</button>
     </div>
   );
 };
